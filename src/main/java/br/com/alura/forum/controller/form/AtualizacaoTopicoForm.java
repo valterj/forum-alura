5907fe540313e1da38f335df3b9f8b367de3d9a3
Valter <valter.zimmermann@totvs.com.br>
2022-01-07 15:36:49 -0300

    Operações CRUD

--- src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -1,22 +1,22 @@
 package br.com.alura.forum.controller.form;
 
+import java.util.Optional;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import br.com.alura.forum.modelo.Topico;
-import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
 
-public class TopicoForm {
+public class AtualizacaoTopicoForm {
 
 	@NotEmpty
-	@Size(min = 5, max = 20)
+	@Size(min = 3, max = 20)
 	private String titulo;
-	
+
 	@NotEmpty
 	@Size(min = 10)
 	private String mensagem;
-	
-	private String nomeCurso;
 
 	public String getTitulo() {
 		return titulo;
@@ -34,17 +34,17 @@ public class TopicoForm {
 		this.mensagem = mensagem;
 	}
 
-	public String getNomeCurso() {
-		return nomeCurso;
-	}
-
-	public void setNomeCurso(String nomeCurso) {
-		this.nomeCurso = nomeCurso;
-	}
-
-	public Topico converter(CursoRepository cursoRepository) {
-		var curso = cursoRepository.findByNome(nomeCurso);
-		return new Topico(titulo, mensagem, curso);
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		Optional<Topico> optional = topicoRepository.findById(id);
+		
+		if (optional.isPresent()) {
+			var topico = optional.get();
+			topico.setTitulo(titulo);
+			topico.setMensagem(mensagem);
+			return topico;
+		}
+		
+		return null;
 	}
 
 }
